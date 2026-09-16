a63a1e4b7e62b78da2ff11a30cc72f3ade3ac0ff
Miguel Angel <miguellopezolias@gmail.com>
2026-09-16 15:39:49 +0200

    init files created

---
+++ generate_mock_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+import random
+
+
+def generate_marketing_data():
+
+    # first we define the possible campaigns name
+    campaigns_name = ["Promo_otoño_26", "Lanzamiento_App", "Retargeting", "Verano_25"]
+
+    # We image data coming from 2 sources: Meta and Google Ads, both with diferent formats:
+    # Meta:
+    
+
+    # Google:
